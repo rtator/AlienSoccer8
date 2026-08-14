5cebeb87974c9590290e945bd4d4af8856e3983b
rtator <hiroshiharrison@gmail.com>
2026-08-14 06:56:14 -0700

    github glitch fix

--- alien-soccer-8/start_screen.gd
+++ alien-soccer-8/start_screen.gd
@@ -2,7 +2,7 @@ extends Control
 
 
 func _on_button_pressed():
-	get_tree().change_scene_to_file("res://alienSelect.tscn")
+	get_tree().change_scene_to_file("res://newAlienSelect.tscn")
 
 
 func _on_button_2_pressed():
