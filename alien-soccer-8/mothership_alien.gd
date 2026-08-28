7df06b7fbda1b8f62839ee3a505f9d524120febd
rtator <hiroshiharrison@gmail.com>
2026-08-27 21:38:23 -0700

    mothership qol

--- alien-soccer-8/mothership_alien.gd
+++ alien-soccer-8/mothership_alien.gd
@@ -8,8 +8,8 @@ var drone_load = preload("res://drone.tscn")
 
 func _ability():
 	if cooldown <= 0 and touching_ore:
-		charge += 5
-		cooldown = 10
+		charge += 8
+		cooldown = 20
 
 func _ultimate():
 	if charge >= charge_max:
@@ -22,9 +22,9 @@ func _ultimate():
 		charge = 0
 
 func _ability_cooldown(delta):
-	if (player == 1 and Input.is_action_pressed("p1_ability")) or (player == 2 and Input.is_action_pressed("p2_ability")):
-		_ability()
-		print("pressed")
+	#if (player == 1 and Input.is_action_pressed("p1_ability")) or (player == 2 and Input.is_action_pressed("p2_ability")):
+		#_ability()
+		#print("pressed")
 	
 	if cooldown > 0:
 		cooldown -= 1 * delta
