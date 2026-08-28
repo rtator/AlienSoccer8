e05aee0eb96ea2f67b42df04c75ef4eaed08bcec
rtator <hiroshiharrison@gmail.com>
2026-08-27 19:31:17 -0700

    mosquito

--- alien-soccer-8/mosquito.gd
+++ alien-soccer-8/mosquito.gd
@@ -13,6 +13,22 @@ var max_swarm_ult_size = 20
 var spawning_swarm_ability = false
 var spawning_swarm_ult = false
 
+var swarm_ult_load = preload("res://swarm_ult.tscn")
+var ult_swarm = []
+
+var ball_scale = 1
+
+var ultimate_duration = 200
+
+var scale_time = 0
+var scale_time_max = 200
+var scaled = false
+
+var original_scale = 0.3
+var scale_mult = 1
+
+var ulting = false
+
 func _ability():
 	if cooldown <= 0:
 		spawning_swarm_ability = true
@@ -20,8 +36,43 @@ func _ability():
 
 func _ultimate():
 	if charge >= charge_max:
-		spawning_swarm_ult = true
+		var angle = 1
+		for i in range(3):
+			
+			var bug = swarm_ult_load.instantiate()
+			
+			bug.hit.connect(bug_hit)
+			bug.rotation_degrees += angle
+			
+			angle += 120
+			
+			ult_swarm.append(bug)
+			add_child(bug)
+		
+		ulting = true
 		charge = 0
+		ult_dur = ultimate_duration
+
+func bug_hit(body):
+	if body == ball:
+		scaled = true
+		scale_time = scale_time_max
+		
+		var bug = swarm_ult_load.instantiate()
+		
+		bug.hit.connect(bug_hit)
+		
+		ult_swarm.append(bug)
+		add_child(bug)
+		
+		
+		ball_scale *= 0.9
+		
+		ball.set_ball_scale(ball_scale)
+		
+		scale_mult += 0.1
+		
+		update_scale(original_scale * scale_mult)
 
 func _ability_cooldown(delta):
 	if spawning_swarm_ability:
@@ -31,7 +82,7 @@ func _ability_cooldown(delta):
 					var swarm = swarm_load.instantiate()
 					var offset = 576 - position.x
 					var offsetVec = Vector2(offset, -abs(offset) * randf_range(0.5,0.7)).normalized()
-					swarm.position = position + (offsetVec * 15)
+					swarm.position = position + (offsetVec * 10)
 					swarm.call_deferred("set_linear_velocity", offsetVec * swarm_speed)
 					add_sibling(swarm)
 					swarm_size_ability += 1
@@ -40,29 +91,53 @@ func _ability_cooldown(delta):
 			swarm_size_ability = 0
 	
 	
-	if spawning_swarm_ult:
-		if swarm_size_ult < max_swarm_ult_size:
-			for i in range(randf_range(1,10)):
-				if swarm_size_ult < max_swarm_ult_size:
-					var swarm = swarm_load.instantiate()
-					var offset = 576 - position.x
-					var offsetVec = Vector2(offset, -abs(offset) * randf_range(0.5,0.7)).normalized()
-					swarm.position = position + (offsetVec * 50)
-					swarm.call_deferred("set_linear_velocity", offsetVec * swarm_speed)
-					add_sibling(swarm)
-					swarm_size_ult += 1
-		else:
-			spawning_swarm_ult = false
-			swarm_size_ult = 0
+	#if spawning_swarm_ult:
+		#if swarm_size_ult < max_swarm_ult_size:
+			#for i in range(randf_range(1,10)):
+				#if swarm_size_ult < max_swarm_ult_size:
+					#var swarm = swarm_load.instantiate()
+					#var offset = 576 - position.x
+					#var offsetVec = Vector2(offset, -abs(offset) * randf_range(0.5,0.7)).normalized()
+					#swarm.position = position + (offsetVec * 50)
+					#swarm.call_deferred("set_linear_velocity", offsetVec * swarm_speed)
+					#add_sibling(swarm)
+					#swarm_size_ult += 1
+		#else:
+			#spawning_swarm_ult = false
+			#swarm_size_ult = 0
+	
+	
 	
 	if cooldown > 0:
 		cooldown -= delta
 	
-	if charge <= charge_max:
+	if charge <= charge_max and ult_dur <= 0:
 		charge += delta
+	
+	if ult_dur > 0:
+		ult_dur -= delta
+	elif ulting:
+		ulting = false
+		
+		for i in ult_swarm:
+			i.queue_free()
+		ult_swarm.clear()
+	
+	if scale_time > 0:
+		scale_time -= delta
+	elif scaled:
+		ball_scale = 1
+		
+		scale_mult = 1
+		
+		update_scale(original_scale)
+		
+		ball.set_ball_scale(1)
+	
+	
 
 func on_ready():
-	base_scale = 0.3
+	base_scale = original_scale
 	
 	if skin != 0:
 		%AnimatedSprite2D.scale = Vector2(0.21, 0.21)
--- alien-soccer-8/swarm_ult.gd
+++ alien-soccer-8/swarm_ult.gd
@@ -1,15 +1,45 @@
 extends Node2D
 
 @onready var body = %body
+@onready var sprite = %sprite
+
+var spin_speed = 3.0
+
+var base_pos = Vector2(0,90)
+
+var buzz_range = 3.0
+var buzz_dir = -1.0
+
+signal hit(body)
 
 func _ready():
-	var rand_numb = randf()
-	var vec = Vector2(randi_range(-40,40), (rand_numb/abs(rand_numb)) * 80)
-	body.position = vec
+	if rotation == 0:
+		rotation_degrees = randi_range(0,360)
 	
-	body.linear_velocity = Vector2(-500,0)
+	spin_speed += randi_range(0, 3)/3
+	
+	body.position *= randf_range(1, 1.7)
+	
+	var rn = randf_range(0.01, 0.4) - 0.25
+	spin_speed *= abs(rn)/rn
+	
+	pass
 
 func _physics_process(delta):
-	body.linear_velocity *= 1.0015
-	
-	body.linear_velocity += (Vector2(0,0) - body.position).normalized() * 50
+	if body != null:
+		body.global_scale = Vector2(1,1)
+		
+		
+		
+		rotation += 1 * delta * spin_speed
+		body.global_rotation = 0
+		
+		body.position = base_pos
+		#body.position.y += buzz_range * buzz_dir
+		#buzz_dir *= -1
+		#body.position += Vector2(randf_range(-buzz_range,buzz_range), randf_range(-buzz_range,buzz_range))
+		#body.scale.y *= -1
+
+
+func _on_body_body_entered(body):
+	emit_signal("hit", body)
