5c561b6a7b1e89f0170fb0b732efda47cc4f16c0
rtator <hiroshiharrison@gmail.com>
2026-09-19 22:59:40 -0700

    3d_v2

---
+++ alien-soccer-8/3d/player/player_camera_3d.gd
@@ -0,0 +1,18 @@
+extends Node3D
+
+var sensitivity = 0.8
+
+var rot_max_y = [-10, 80]
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		var input = event.screen_relative
+		rotation_degrees.y += -input.x * sensitivity
+		rotation_degrees.x += -input.y * sensitivity
+		rotation_degrees.x = clamp(rotation_degrees.x, rot_max_y[0], rot_max_y[1])
+	
+	if event is InputEventMouseButton:
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	
+	if event is InputEventKey and event.is_action_pressed("ui_cancel") and event.is_pressed():
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
